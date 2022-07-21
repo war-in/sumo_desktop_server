c8971b3ac723fb517bd86aea7fd02a6973b1b50a
war-in <marcin.warchol20@gmail.com>
2022-07-21 17:49:05 +0200

    it is working! (at the first time)

--- src/main/java/sumo/desktop_server/Controllers/Draw/DrawController.java
+++ src/main/java/sumo/desktop_server/Controllers/Draw/DrawController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import sumo.desktop_server.Controllers.Utils.Draw.CategoryAndCompetitors;
 import sumo.desktop_server.Database.Competition.CompetitionService;
+import sumo.desktop_server.Database.DrawType.DrawType;
+import sumo.desktop_server.Database.DrawType.DrawTypeService;
+import sumo.desktop_server.Database.Region.Region;
 
 import java.util.List;
 
@@ -17,6 +20,7 @@ import java.util.List;
 public class DrawController {
 
     private final CompetitionService competitionService;
+    private final DrawTypeService drawTypeService;
 
     @GetMapping("/get-categories-with-competitors")
     public ResponseEntity<List<CategoryAndCompetitors>> getCategoriesWithCompetitors(@RequestParam Long competitionId) {
@@ -24,4 +28,12 @@ public class DrawController {
 
         return ResponseEntity.ok().body(categoriesAndCompetitors);
     }
+
+    @GetMapping("/get-suggested-draw-types")
+    public ResponseEntity<List<DrawType>> getSuggestedDrawTypes(@RequestParam Long numberOfCompetitors, @RequestParam String region) {
+        List<DrawType> drawTypes = drawTypeService.getDrawTypesAboveSpecifiedNumberOfCompetitors(numberOfCompetitors, region);
+
+        return ResponseEntity.ok().body(drawTypes);
+    }
+
 }
