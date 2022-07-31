df9f45deed5cb3edb621fda785a76196a85940fb
Barbara Kulawska <kulawskab@student.agh.edu.pl>
2022-07-31 18:26:07 +0200

    Fixed CORS for front access

--- src/main/java/sumo/desktop_server/DesktopServerApplication.java
+++ src/main/java/sumo/desktop_server/DesktopServerApplication.java
@@ -2,12 +2,27 @@ package sumo.desktop_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class DesktopServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DesktopServerApplication.class, args);
+    }
+
+    @Configuration
+    @EnableWebMvc
+    public static class WebConfig implements WebMvcConfigurer {
+
+        @Override
+        public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/**")
+                    .allowedOrigins("*");
+        }
     }
 
 }
